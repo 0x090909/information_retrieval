8476a65a811d0a039eab3a864ec57e74db064173
asteph7cd <50109292+asteph7cd@users.noreply.github.com>
2019-05-01 10:35:22 +0200

    Update query_expansion_feedback_batch.py
    cambiato alias da "str" a "st"

--- ricerca/query_expansion_feedback_batch.py
+++ ricerca/query_expansion_feedback_batch.py
@@ -1,5 +1,5 @@
 #--- importazione di interi moduli
-import string as str
+import string as st
 import sys
 import getopt
 import os.path
@@ -24,7 +24,7 @@ def gettagdata(dom,tag):
         return None
     tagdata = []
     for node in nodes:
-        tagdata.append(str.rstrip(str.lstrip(node.firstChild.data)))
+        tagdata.append(st.rstrip(st.lstrip(node.firstChild.data)))
     return tagdata
 
 # ---------------------------------------------------------------------------------------- #
