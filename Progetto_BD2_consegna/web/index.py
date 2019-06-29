2e2093269c42aa2e0c672f42a110472b4a12d9a4
asteph7cd <alessandro.stefani.6@studenti.unipd.it>
2019-06-29 11:32:08 +0200

    aggiunta cartella web vabbe' bisognerà decidere cosa fare

---
+++ Progetto_BD2_consegna/web/index.py
@@ -0,0 +1,61 @@
+import web
+from whoosh.index import create_in
+from whoosh.fields import *
+from whoosh import qparser
+from whoosh import scoring
+from whoosh.qparser import QueryParser
+from whoosh.filedb.filestore import FileStorage
+from paginate_whoosh import WhooshPage
+
+render = web.template.render('templates')
+
+#inizializzo due documenti
+schema = Schema(docid      	= ID(stored=True),
+				title      	= TEXT(stored=True),
+				identifier	= ID(stored=True),
+				terms 		= NGRAM(stored=True),
+				authors     = NGRAM(stored=True),
+				abstract 	= TEXT(stored=True),
+				publication	= TEXT(stored=True),
+				source 		= TEXT(stored=True))
+st = FileStorage("../ohsumed_index_dir_stopwords_clinico")
+ix = st.open_index()
+
+urls = (
+	'/','index',
+	'/index', 'index',
+	'/search', 'search',
+	'/article', 'article'
+
+)
+
+class index:
+	def GET(self):
+		return render.index()
+
+class article:
+	def GET(self):
+		user_data = web.input()
+		#user_data.docid
+		return render.post(docid)
+
+class search:
+	def GET(self):
+		user_data = web.input()
+
+		titles = '<ol>'
+		with ix.searcher() as searcher:
+			q = QueryParser("title",schema, group=qparser.OrGroup).parse(user_data.query)
+			searcher = ix.searcher(weighting=scoring.TF_IDF()).search(q, limit=1000)
+			page = WhooshPage(
+				searcher, # limit=None is required!
+				page=user_data.page, items_per_page=10)
+
+		searcherlen = len(searcher)
+		out = [{"abstract":article.get('abstract')[:300] + '...' if "abstract" in article.keys() and len(article.get('abstract')) > 300   else article.get('abstract'), "docid":article.get("docid"),"title":article.get("title")} for article in page]
+		pages = range(1,max(2,searcherlen/10+1))
+		return render.searchResults(out, user_data, searcherlen, pages)
+
+if __name__ == "__main__":
+	app = web.application(urls, globals())
+	app.run()
---
+++ Progetto_BD2_consegna/web/index_1.py
@@ -0,0 +1,49 @@
+import web
+from whoosh.index import create_in
+from whoosh.fields import *
+from whoosh import qparser
+from whoosh import scoring
+from whoosh.qparser import QueryParser
+from whoosh.filedb.filestore import FileStorage
+from paginate_whoosh import WhooshPage
+from search_ohsumed import src
+import re
+
+render = web.template.render('templates',base="main_layout",globals={'re':re})
+
+
+fst = FileStorage("../ohsumed_index_dir_stopwords_clinico")
+
+urls = (
+	'/','index',
+	'/index', 'index',
+	'/search', 'search',
+	'/article', 'article'
+	#'/advancedsearch', 'adv_search' ## PREMIUM FEATURE
+)
+
+class index:
+	def GET(self):
+		return render.index()
+
+class article:
+	def GET(self):
+		user_data = web.input()
+		#user_data.docid
+		return render.post(docid)
+
+class search:
+	def GET(self):
+		user_data = web.input()
+		out, searcherlen, pages = src(fst,user_data,lim=1000)
+		if searcherlen:
+			return render.searchResults(out, user_data, searcherlen, pages)
+		else:
+			return render.searchResults(out, user_data,0,None)
+class adv_search:
+	def GET(self):
+		return render.advanced()
+
+if __name__ == "__main__":
+	app = web.application(urls, globals())
+	app.run()
